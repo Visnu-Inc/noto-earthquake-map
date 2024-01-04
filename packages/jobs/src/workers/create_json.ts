6254ba11c0271b3a959f6a0243321dc3fafd2064
kohta ito <kohta110@gmail.com>
2024-01-04 21:06:16 +0900

    add road

--- packages/jobs/src/workers/create_json.ts
+++ packages/jobs/src/workers/create_json.ts
@@ -1,14 +1,8 @@
-import { writeFile, readFile } from 'node:fs/promises'
+import { writeFile, readFile, mkdtemp, rm } from 'node:fs/promises'
 import { fileURLToPath } from 'node:url'
 import path from 'node:path'
 import { parse } from 'csv'
-import {
-  S3Client,
-  ListBucketsCommand,
-  ListObjectsV2Command,
-  GetObjectCommand,
-  PutObjectCommand
-} from '@aws-sdk/client-s3'
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
 import { CLOUDFLARE } from '../config.js'
 
 const s3 = new S3Client({
@@ -20,14 +14,68 @@ const s3 = new S3Client({
   }
 })
 
-const dirname = path.dirname(fileURLToPath(import.meta.url))
+const url = 'https://docs.google.com/spreadsheets/d/1Wa3EltKUwq2-d8W8s6QlJ7TIEC83kc8131xuX9q_5OI'
 
 export async function createJson() {
-  const res = await fetch('https://docs.google.com/spreadsheets/d/1Wa3EltKUwq2-d8W8s6QlJ7TIEC83kc8131xuX9q_5OI/export?gid=0&format=tsv')
-  const downloadData = Buffer.from(await res.arrayBuffer())
+  const dirname = path.dirname(fileURLToPath(import.meta.url))
+  const tempdir = await mkdtemp(path.join(dirname, './tmp-'))
 
-  const downloadDataPath = path.join(dirname, './data.tsv')
-  await writeFile(downloadDataPath, downloadData)
+  const res = await Promise.allSettled([ 
+    createInfo(tempdir).then((data) => upload('info.json', JSON.stringify(data))),
+    createRoadInfo(tempdir).then((data) => upload('road.json', JSON.stringify(data)))
+  ])
+  console.log(res)
+
+  await rm(tempdir, { recursive: true })
+}
+
+async function createRoadInfo(tmpdirPath: string) {
+  const query = new URLSearchParams([
+    ['gid', '525552252'],
+    ['format', 'tsv'],
+  ])
+  const downloadDataPath = path.join(tmpdirPath, './road-data.tsv')
+  await download(`${url}/export?${query.toString()}`, downloadDataPath)
+
+  const tsv = await readFile(downloadDataPath, 'utf8')
+  const parser = parse(tsv, {
+    delimiter: '\t',
+    trim: true,
+    from_line: 1
+  })
+
+  const data: {
+    状況: string
+    最終更新時刻: string
+    情報源: string
+    others: string
+  }[] = []
+
+  for await (const record of parser) {
+    const [状況, 最終更新時刻, 情報源, ...others] = record
+    data.push({
+      状況,
+      最終更新時刻,
+      情報源,
+      others: others.filter((e: unknown) => !!e).join('\n')
+    })
+  }
+
+  console.log(JSON.stringify({
+    label: 'road',
+    payload: data
+  }))
+
+  return { data }
+}
+
+async function createInfo(tmpdirPath: string) {
+  const query = new URLSearchParams([
+    ['gid', '0'],
+    ['format', 'tsv'],
+  ])
+  const downloadDataPath = path.join(tmpdirPath, './info-data.tsv')
+  await download(`${url}/export?${query.toString()}`, downloadDataPath)
 
   const tsv = await readFile(downloadDataPath, 'utf8')
   const parser = parse(tsv, {
@@ -36,7 +84,7 @@ export async function createJson() {
     from_line: 2
   })
 
-  const info: {
+  const data: {
     id: string
     市町村: string
     市町村2: string
@@ -59,7 +107,7 @@ export async function createJson() {
     const id = _id || before.id
     const 市町村 = _市町村 || before.市町村
     const 市町村2 = _市町村2 || before.市町村2
-    info.push({
+    data.push({
       id,
       市町村,
       市町村2,
@@ -75,16 +123,27 @@ export async function createJson() {
     before.市町村 = 市町村
     before.市町村2 = 市町村2
   }
+  console.log(JSON.stringify({
+    label: 'info',
+    payload: data
+  }))
 
-  const uploadDataPath = path.join(dirname, './info.json')
-  await writeFile(uploadDataPath, JSON.stringify(info))
+  return { data }
+}
 
+async function download(url: string, path: string) {
+  const res = await fetch(url)
+  const downloadData = Buffer.from(await res.arrayBuffer())
+  await writeFile(path, downloadData)
+}
+
+async function upload(key: string, body: string) {
   const command = new PutObjectCommand({
-    Key: 'info.json',
-    Body: JSON.stringify(info),
+    Key: key,
+    Body: JSON.stringify(body),
     ContentType: 'application/json',
     Bucket: CLOUDFLARE.R2_BUCKET
   })
   const data = await s3.send(command)
-  console.log(data)
+  return data
 }
