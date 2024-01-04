c164d183df029109cc3f8f1eb68bc9dbcb99c552
kohta ito <kohta110@gmail.com>
2024-01-04 21:22:17 +0900

    add other page

--- packages/jobs/src/workers/create_json.ts
+++ packages/jobs/src/workers/create_json.ts
@@ -22,51 +22,30 @@ export async function createJson() {
 
   const res = await Promise.allSettled([ 
     createInfo(tempdir).then((data) => upload('info.json', JSON.stringify(data))),
-    createRoadInfo(tempdir).then((data) => upload('road.json', JSON.stringify(data)))
+    createRoadInfo(tempdir).then((data) => upload('road.json', JSON.stringify(data))),
+    createSupportInfo(tempdir).then((data) => upload('support.json', JSON.stringify(data))),
+    createStoreInfo(tempdir).then((data) => upload('store.json', JSON.stringify(data)))
   ])
   console.log(res)
 
   await rm(tempdir, { recursive: true })
 }
 
-async function createRoadInfo(tmpdirPath: string) {
-  const query = new URLSearchParams([
-    ['gid', '525552252'],
-    ['format', 'tsv'],
-  ])
-  const downloadDataPath = path.join(tmpdirPath, './road-data.tsv')
-  await download(`${url}/export?${query.toString()}`, downloadDataPath)
+async function download(url: string, path: string) {
+  const res = await fetch(url)
+  const downloadData = Buffer.from(await res.arrayBuffer())
+  await writeFile(path, downloadData)
+}
 
-  const tsv = await readFile(downloadDataPath, 'utf8')
-  const parser = parse(tsv, {
-    delimiter: '\t',
-    trim: true,
-    from_line: 1
+async function upload(key: string, body: string) {
+  const command = new PutObjectCommand({
+    Key: key,
+    Body: body,
+    ContentType: 'application/json',
+    Bucket: CLOUDFLARE.R2_BUCKET
   })
-
-  const data: {
-    状況: string
-    最終更新時刻: string
-    情報源: string
-    others: string
-  }[] = []
-
-  for await (const record of parser) {
-    const [状況, 最終更新時刻, 情報源, ...others] = record
-    data.push({
-      状況,
-      最終更新時刻,
-      情報源,
-      others: others.filter((e: unknown) => !!e).join('\n')
-    })
-  }
-
-  console.log(JSON.stringify({
-    label: 'road',
-    payload: data
-  }))
-
-  return { data }
+  const data = await s3.send(command)
+  return data
 }
 
 async function createInfo(tmpdirPath: string) {
@@ -131,19 +110,120 @@ async function createInfo(tmpdirPath: string) {
   return { data }
 }
 
-async function download(url: string, path: string) {
-  const res = await fetch(url)
-  const downloadData = Buffer.from(await res.arrayBuffer())
-  await writeFile(path, downloadData)
+async function createRoadInfo(tmpdirPath: string) {
+  const query = new URLSearchParams([
+    ['gid', '525552252'],
+    ['format', 'tsv'],
+  ])
+  const downloadDataPath = path.join(tmpdirPath, './road-data.tsv')
+  await download(`${url}/export?${query.toString()}`, downloadDataPath)
+
+  const tsv = await readFile(downloadDataPath, 'utf8')
+  const parser = parse(tsv, {
+    delimiter: '\t',
+    trim: true,
+    from_line: 1
+  })
+
+  const data: {
+    状況: string
+    最終更新時刻: string
+    情報源: string
+    others: string
+  }[] = []
+
+  for await (const record of parser) {
+    const [状況, 最終更新時刻, 情報源, ...others] = record
+    data.push({
+      状況,
+      最終更新時刻,
+      情報源,
+      others: others.filter((e: unknown) => !!e).join('\n')
+    })
+  }
+
+  console.log(JSON.stringify({
+    label: 'road',
+    payload: data
+  }))
+
+  return { data }
 }
 
-async function upload(key: string, body: string) {
-  const command = new PutObjectCommand({
-    Key: key,
-    Body: JSON.stringify(body),
-    ContentType: 'application/json',
-    Bucket: CLOUDFLARE.R2_BUCKET
+async function createSupportInfo(tmpdirPath: string) {
+  const query = new URLSearchParams([
+    ['gid', '1768926558'],
+    ['format', 'tsv'],
+  ])
+  const downloadDataPath = path.join(tmpdirPath, './support-data.tsv')
+  await download(`${url}/export?${query.toString()}`, downloadDataPath)
+
+  const tsv = await readFile(downloadDataPath, 'utf8')
+  const parser = parse(tsv, {
+    delimiter: '\t',
+    trim: true,
+    from_line: 2
   })
-  const data = await s3.send(command)
-  return data
+
+  const data: {
+    title: string
+    情報源: string
+    others: string
+  }[] = []
+
+  for await (const record of parser) {
+    const [title, 情報源, ...others] = record
+    data.push({
+      title,
+      情報源,
+      others: others.filter((e: unknown) => !!e).join('\n')
+    })
+  }
+
+  console.log(JSON.stringify({
+    label: 'support',
+    payload: data
+  }))
+
+  return { data }
+}
+
+async function createStoreInfo(tmpdirPath: string) {
+  const query = new URLSearchParams([
+    ['gid', '1768926558'],
+    ['format', 'tsv'],
+  ])
+  const downloadDataPath = path.join(tmpdirPath, './store-data.tsv')
+  await download(`${url}/export?${query.toString()}`, downloadDataPath)
+
+  const tsv = await readFile(downloadDataPath, 'utf8')
+  const parser = parse(tsv, {
+    delimiter: '\t',
+    trim: true,
+    from_line: 2
+  })
+
+  const data: {
+    番号: string
+    地域: string
+    店舗名: string
+    others: string
+  }[] = []
+
+  for await (const record of parser) {
+    const [番号, 地域, 店舗名, ...others] = record
+    data.push({
+      番号,
+      地域,
+      店舗名,
+      others: others.filter((e: unknown) => !!e).join('\n')
+    })
+  }
+
+  console.log(JSON.stringify({
+    label: 'store',
+    payload: data
+  }))
+
+  return { data }
 }
